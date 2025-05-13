0c0277e562e699efb17cbce64bfde3063da148cb
Kojo <kodeon6@gmail.com>
2025-05-13 18:44:54 +0100

    Add header and footer with responsive design

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,18 @@ Rails.application.routes.draw do
   
   resources :products, only: [:index, :show]
   resources :articles, only: [:index, :show]
+  resources :categories, only: [:index, :show]
   
   namespace :admin do
     resources :products
     resources :articles
+    resources :categories
   end
   
   # Affiliate link tracking
   get '/go/:id', to: 'affiliate_links#redirect', as: :affiliate_redirect
+
+
+  # PWA manifest
+  # get '/manifest.json', to: 'pwa#manifest', as: :pwa_manifest
 end
